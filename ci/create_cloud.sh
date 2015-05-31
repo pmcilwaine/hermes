b76f48c8eaba2b7c3de9c1cf763b42117f15e950
Paul Mcilwaine <paul.mcilwaine@gmail.com>
2015-05-31 17:14:36 +1000

    HER-61 Installation fixes

--- ci/create_cloud.sh
+++ ci/create_cloud.sh
@@ -1,1 +1,42 @@
 #!/usr/bin/env bash
+
+set -xe
+
+# this creates a single instance for now, everything is already configured
+
+while [ $# -gt 0 ]; do
+
+    arg=$1
+
+    case $arg in
+
+        -h|--help)
+            help_text
+        ;;
+        -v|--version)
+            export VERSION="$2"
+            shift; shift
+        ;;
+        *)
+            echo "ERROR: Unrecognised option: ${arg}"
+            help_text
+            exit 1
+        ;;
+    esac
+done
+
+ROOT_DIR=$(readlink -m $(dirname $0)/..)
+cd ${ROOT_DIR}/baking
+
+packer build -var version=${VERSION} -var base_ami=ami-f7740dcd -var image_name=hermes_cms one_role.json
+
+# get AMI
+IMAGE_ID=$(aws ec2 describe-images --filters Name=name,Values=hermes_cms_${VERSION} | jq -r '.Images[0].ImageId')
+
+INSTANCE_ID=$(aws ec2 run-instances --image-id ${IMAGE_ID} --key-name pmcilwaine-aws --iam-instance-profile Name=instance-role --count 1  --instance-type t2.micro --security-group-ids sg-73d24616 | jq -r ".Instances[0].InstanceId")
+
+until [ `aws ec2 describe-instances --instance-ids ${INSTANCE_ID} | jq -r '.Reservations[0].Instances[0].State.Name'` == "running" ]; do
+    echo "Waiting for instance to get up"
+done
+
+aws ec2 create-tags --resource ${INSTANCE_ID} --tags Key=Name,Value=hermes_cms_${VERSION}
--- ci/delete_cloud.sh
+++ ci/delete_cloud.sh
@@ -1,1 +1,27 @@
 #!/usr/bin/env bash
+
+# this creates a single instance for now, everything is already configured
+
+while [ $# -gt 0 ]; do
+
+    arg=$1
+
+    case $arg in
+
+        -h|--help)
+            help_text
+        ;;
+        -v|--version)
+            export VERSION="$2"
+            shift; shift
+        ;;
+        *)
+            echo "ERROR: Unrecognised option: ${arg}"
+            help_text
+            exit 1
+        ;;
+    esac
+done
+
+INSTANCE_ID=$(aws ec2 describe-instances --filter Name=tag:Name,Values=hermes_cms_${VERSION} | jq -r '.Reservations[0].Instances[0].InstanceId')
+aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
