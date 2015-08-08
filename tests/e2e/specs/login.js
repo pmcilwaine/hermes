64331b452328df2751a999efbace654e9185fef9
Paul Mcilwaine <paul.mcilwaine@gmail.com>
2015-08-08 21:32:45 +1000

    HER-75 Setup E2E

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -10,24 +10,51 @@ describe('Login', function () {
 
     before(function () {
         isAngularSite(false);
-    })
-
-    beforeEach(function () {
-        browser.get('/login');
     });
 
-    it('Test Invalid Login', function () {
-        element.all(by.css('#email')).get(0).sendKeys("invalid-user@example.org");
-        element.all(by.css('#password')).get(0).sendKeys("5a4d5a4dasda dasdasd");
-        element.all(by.css('form button[type=submit]')).click();
-        expect(element.all(by.css('.alert')).count()).to.eventually.equal(1);
+    describe('Login to Site', function () {
+
+        beforeEach(function () {
+            browser.get('/login');
+        });
+
+        it('Test Invalid Login', function () {
+            element.all(by.css('#email')).get(0).sendKeys("invalid-user@example.org");
+            element.all(by.css('#password')).get(0).sendKeys("5a4d5a4dasda dasdasd");
+            element.all(by.css('form button[type=submit]')).click();
+            expect(element.all(by.css('.alert')).count()).to.eventually.equal(1);
+        });
+
+        it('Test Valid Login', function () {
+            element.all(by.css('#email')).get(0).sendKeys("test@example.org");
+            element.all(by.css('#password')).get(0).sendKeys("password");
+            element.all(by.css('form button[type=submit]')).click();
+            expect(browser.getCurrentUrl()).to.eventually.equal(process.env.BASE_URL + '/');
+        });
+
+        afterEach(function () {
+            browser.get('/logout');
+        });
+
     });
 
-    it('Test Valid Login', function () {
-        element.all(by.css('#email')).get(0).sendKeys("test@example.org");
-        element.all(by.css('#password')).get(0).sendKeys("password");
-        element.all(by.css('form button[type=submit]')).click();
-        expect(browser.getCurrentUrl()).to.eventually.equal(process.env.BASE_URL + '/');
+    describe('Login to Admin', function () {
+
+        beforeEach(function () {
+            browser.get('/admin/');
+        });
+
+        it('Test redirected to login', function () {
+            expect(browser.getCurrentUrl()).to.eventually.equal(process.env.BASE_URL + '/login?next_page=');
+        });
+
+        it('Not logged Administrator with redirection', function () {
+            element.all(by.css('#email')).get(0).sendKeys("test@example.org");
+            element.all(by.css('#password')).get(0).sendKeys("password");
+            element.all(by.css('form button[type=submit]')).click();
+            expect(browser.getCurrentUrl()).to.eventually.equal(process.env.BASE_URL + '/admin/');
+        });
+
     });
 
 });
---
+++ tests/e2e/specs/view-document.js
@@ -0,0 +1,27 @@
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+
+chai.should();
+var expect = chai.expect;
+
+var helpers = require('../../helpers/helpers.js');
+
+describe('View Document', function () {
+
+    before(function () {
+        isAngularSite(false);
+    });
+
+    it('Can View Document', function () {
+
+    });
+
+    it('404 Page Displayed', function () {
+
+    });
+
+    it('Can download a File Type Document', function () {
+
+    });
+
+});
